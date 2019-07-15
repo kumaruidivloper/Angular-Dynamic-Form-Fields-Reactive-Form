49ae54a9032e5a7882a81f13a2e3c34bb2b98077
kumarshan <kumaruidivloper@gmail.com>
2019-07-15 16:25:13 +0530

    POC-Get-Checkbox-checked-value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,161 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { formControlBinding } from '@angular/forms/src/directives/ng_model';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  public title = 'Angular-Dynamic-Form-Fields-Reactive-Form';
-  public form: FormGroup;
-  public contactList: FormArray;
-  public positionList: FormArray;
-  public country: string[] =  ['India', 'Singapore', 'England', 'Pakistan', 'UAE', 'South Africa'];
-  emails = [{ language: 'English' }, { language: 'Tamil' }, { language: 'Germany' }, { language: 'Spanish' }];
+export class AppComponent {
+  form: FormGroup;
+  orders = [];
 
-  // returns all form groups under contacts
-  get contactFormGroup() {
-    return this.form.get('contacts') as FormArray;
-  }
-
-  // returns all form groups under position
-  get positionFormGroup() {
-    return this.form.get('position') as FormArray;
-  }
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.form = this.fb.group({
-      name: [null, Validators.compose([Validators.required])],
-      organization: [null],
-      contacts: this.fb.array([this.createContact()]),
-      position: this.fb.array([this.createPosition()])
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      orders: new FormArray([], minSelectedCheckboxes(1))
     });
 
-    // set contactlist to this field
-    this.contactList = this.form.get('contacts') as FormArray;
-    this.positionList = this.form.get('position') as FormArray;
+    // async orders
+    of(this.getOrders()).subscribe(orders => {
+      this.orders = orders;
+      this.addCheckboxes();
+    });
+
+    // synchronous orders
+    // this.orders = this.getOrders();
+    // this.addCheckboxes();
   }
 
-  // contact formgroup
-  createContact(): FormGroup {
-    return this.fb.group({
-      type: ['email', Validators.compose([Validators.required])], // i.e Email, Phone
-      name: [null, Validators.compose([Validators.required])], // i.e. Home, Office
-      value: [null, Validators.compose([Validators.required, Validators.email])],
-      country: [''],
-      gender: [''],
-      language: this.fb.array([])
+  private addCheckboxes() {
+    this.orders.map((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.form.controls.orders as FormArray).push(control);
     });
   }
 
-  // Position formgroup
-  createPosition(): FormGroup {
-    return this.fb.group({
-      type: ['email', Validators.compose([Validators.required])], // i.e Email, Phone
-      name: [null, Validators.compose([Validators.required])], // i.e. Home, Office
-      value: [null, Validators.compose([Validators.required, Validators.email])]
-    });
+  getOrders() {
+    return [
+      { id: 100, name: 'order 1' },
+      { id: 200, name: 'order 2' },
+      { id: 300, name: 'order 3' },
+      { id: 400, name: 'order 4' }
+    ];
   }
 
-  // add a contact form group
-  addContact() {
-    this.contactList.push(this.createContact());
-  }
-
-  // add a position form group
-  addPosition() {
-    this.positionList.push(this.createPosition());
-  }
-
-  // remove contact from group
-  removeContact(index) {
-    // this.contactList = this.form.get('contacts') as FormArray;
-    this.contactList.removeAt(index);
-  }
-
-  // remove position from group
-  removePostion(index) {
-    // this.contactList = this.form.get('contacts') as FormArray;
-    console.log(this.positionList.length);
-    this.positionList.removeAt(index);
-  }
-
-  // triggered to change validation of value field type
-  changedFieldContactType(index) {
-    let validators = null;
-
-    if (this.getContactsFormGroup(index).controls.type.value === 'email') {
-      validators = Validators.compose([Validators.required, Validators.email]);
-    } else {
-      validators = Validators.compose([
-        Validators.required,
-        Validators.pattern(new RegExp('^\\+[0-9]?()[0-9](\\d[0-9]{9})$')) // pattern for validating international phone number
-      ]);
-    }
-
-    this.getContactsFormGroup(index).controls.value.setValidators(
-      validators
-    );
-
-
-    this.getContactsFormGroup(index).controls.value.updateValueAndValidity();
-  }
-
-
-  changedFieldPositionType(index) {
-    let validators = null;
-    if (this.getPositionFormGroup(index).controls.type.value === 'email') {
-      validators = Validators.compose([Validators.required, Validators.email]);
-    } else {
-      validators = Validators.compose([
-        Validators.required,
-        Validators.pattern(new RegExp('^\\+[0-9]?()[0-9](\\d[0-9]{9})$')) // pattern for validating international phone number
-      ]);
-    }
-
-    this.getPositionFormGroup(index).controls.value.setValidators(
-      validators
-    );
-
-    this.getPositionFormGroup(index).controls.value.updateValueAndValidity();
-  }
-
-  // get the formgroup under contacts form array
-  getContactsFormGroup(index): FormGroup {
-    // this.contactList = this.form.get('contacts') as FormArray;
-    const formGroup = this.contactList.controls[index] as FormGroup;
-    return formGroup;
-  }
-
-  // get the formgroup under contacts form array
-  getPositionFormGroup(index): FormGroup {
-    // this.contactList = this.form.get('contacts') as FormArray;
-    const formGroup = this.positionList.controls[index] as FormGroup;
-    return formGroup;
-  }
-
-  onChange(language: string, isChecked: boolean) {
-    console.log(language);
-    // const emailFormArray = <FormArray>this.form.controls.useremail;
-    const CheckedValue = this.form.get('contacts').value as FormArray;
-    const emailFormArray = CheckedValue[0].language;
-    console.log(emailFormArray);
-    if (isChecked) {
-      emailFormArray.push(language);
-      console.log(emailFormArray);
-    } else {
-      const index = emailFormArray.indexOf(language);
-      console.log(index);
-      emailFormArray.splice(index, 1);
-    }
-  }
-
-  // method triggered when form is submitted
   submit() {
-    console.log(this.form.value);
+    const selectedOrderIds = this.form.value.orders
+      .map((v, i) => v ? this.orders[i].id : null)
+      .filter(v => v !== null);
+    console.log(selectedOrderIds);
   }
+}
+
+function minSelectedCheckboxes(min = 1) {
+  const validator: ValidatorFn = (formArray: FormArray) => {
+    const totalSelected = formArray.controls
+      .map(control => control.value)
+      .reduce((prev, next) => next ? prev + next : prev, 0);
+
+    return totalSelected >= min ? null : { required: true };
+  };
+
+  return validator;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, ReactiveFormsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
